79b59e22b88e4997e82381374f5a608359fcb42f
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-12-12 11:10:51 -0300

    Update

--- Reference-and-Value-Types/Reference and Value Types Practice 01/Program.cs
+++ Reference-and-Value-Types/Reference and Value Types Practice 01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//------Programa Principial------
+
+// Define two numbers
+int number01 = 5;
+int number02 = 3;
+
+//Print the values before swapping
+Console.WriteLine("Values before swapping: {0} {1}", number01, number02);
+
+// Save the content (copy) of number01 on temp
+int temp = number01;
+// now we can safely change the value of number01 since we have a copy of it in temp
+number01 = number02;
